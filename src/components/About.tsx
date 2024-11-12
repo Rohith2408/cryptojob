7b1e17f813d947af837e460d62b2ae1253051440
Rohith2408 <kumarrohith24081999@gmail.com>
2024-11-12 11:53:07 +0530

    added links

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -12,7 +12,7 @@ const About=()=>{
                         <p className={styles.title} style={{color:"#F35701"}}>Us</p>
                     </div>
                     <p className={styles.text}>
-                    Welcome to Crypto Jobs Hub, the go-to platform for finding your next role in the crypto space, managed by @GrindingPoet and @Pancakesbrah. We’re here to make job applications easy and accessible—no more waiting in DMs or searching through scattered posts. Just fill out our simple application form, and we’ll follow up with you as soon as possible.
+                    Welcome to Crypto Jobs Hub, the go-to platform for finding your next role in the crypto space, managed by <a href="https://x.com/grindingpoet">@GrindingPoet</a> and <a href="https://x.com/pancakesbrah">@Pancakesbrah</a>. We’re here to make job applications easy and accessible—no more waiting in DMs or searching through scattered posts. Just fill out our simple application form, and we’ll follow up with you as soon as possible.
                     Whether you’re new to crypto or an experienced pro, we connect you with top industry opportunities across roles like development, marketing, and community management. Start building your future in crypto with us!
                     </p>
                 </div>
